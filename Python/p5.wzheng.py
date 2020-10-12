812846be626948b37ded96e68a5fd3e60e8272f1
weitheway <hiwei103098@gmail.com>
2020-10-12 02:03:19 -0400

    P5 - String Manipulation

---
+++ Python/p5.wzheng.py
@@ -0,0 +1,27 @@
+""" 
+    Author: Wen Wei Zheng
+    Course: SSW 540
+
+    Assignment: P5 - String Manipulation
+
+"""
+
+def plural(s):
+    words = s
+    for i in range(len(words)):
+        if words[i].endswith(('oy','ey','ay')):
+            words[i] = words[i] + "s"
+        elif words[i].endswith('y'):
+            words[i] = words[i][:-1] + "ies"
+        elif words[i].endswith(('o','ch','s','sh','x','z')):
+            words[i] = words[i] + "es"
+        else:
+            words[i] = words[i] + "s"
+    return words
+repeat = "1"
+while repeat == "1":
+    line = input("Please input words separated by a space: ")
+    lines = line.split(" ")
+    pline = " ".join(plural(lines))
+    print(pline)
+    repeat = input("Press 1 to input another set of words. 0 to quit.")
