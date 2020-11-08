55635051251727103b0b9951806a0b732fad5b69
weitheway <hiwei103098@gmail.com>
2020-11-08 02:25:44 -0500

    Finished P8

--- Python/p8.wzheng.py
+++ Python/p8.wzheng.py
@@ -2,7 +2,62 @@
     Author: Wen Wei Zheng
     Course: SSW 540
 
-    Assignment: P8 - 
-
+    Assignment: P8 - Classic Books
 """
 
+from string import punctuation
+from operator import itemgetter
+from collections import defaultdict
+ 
+run = "1"
+
+while run == "1":
+    # Making sure the input file exists 
+    try:
+        filename = input("What file would you like to open? ")
+        fhand = open(filename)
+    except:
+        print("Error: File '" +filename+ "' was not found")
+        run = input("Press 1 to open another file. Anything else to quit. ")
+        continue
+    
+    # Declaring the variables
+    wordcount = 0
+    d = defaultdict(int)
+    charFreq = defaultdict(int)
+    
+    #Removes punctuation from each line first
+    #Lowercase each line
+    #Split each line 
+    #Increment wordcount for each word
+    #Use the dict to count words
+    #Use another dict to count each character
+    try:
+        for line in fhand:
+            punc_translator = line.maketrans({key: None for key in punctuation})
+            cleanString = line.translate(punc_translator)
+            words = cleanString.lower()
+            words = words.split()
+            for word in words:
+                wordcount +=1
+                d[word] +=1
+                for cha in word:
+                    charFreq[cha] +=1
+    except:
+        print("Error detected")
+
+    # File Output
+    print("\nHere's a summary of the file you have selected: \n")
+    if d != {}:
+        print("There are {:,} total words with {} distinct words in this file.\n".format(wordcount,len(d)))
+        ws = sorted(d.items(),key=itemgetter(1), reverse=True)
+        print("The top 25 frequent words and their frequency are as follows: ")
+        print(ws[:25])
+        print("\n")
+        cs = sorted(charFreq.items(),key=itemgetter(1), reverse=True)
+        print("The most frequently used characters to least frequently used characters are as follows: ")
+        print(cs)
+        print("Note: Any letter not shown above was not used in this file.\n")
+    else:
+        print("The file you selected is an empty file.")
+    run = input("Press 1 to input another file. Anything else to quit. ")
--- Python/test.py
+++ Python/test.py
@@ -2,10 +2,19 @@
     Author: Wen Wei Zheng
     Course: SSW 540
 
-    Assignment: P7 - Finding and Counting Unique Items
+    Assignment: P8 - Classic Books
+The summary should include:
 
+Total words
+Total distinct words
+The top 25 most frequent words and counts (You do NOT need to handle ties.  Just pick the top 25)
+Character frequency sorted from most frequent to least frequent characters
 """
 
+from string import punctuation
+from operator import itemgetter
+from collections import defaultdict
+ 
 run = "1"
 
 while run == "1":
@@ -14,24 +23,39 @@ while run == "1":
         filename = input("What file would you like to open? ")
         fhand = open(filename)
     except:
-        print("File " +filename+ " was not found")
+        print("Error: File '" +filename+ "' was not found")
         run = input("Press 1 to open another file. Anything else to quit. ")
         continue
     
-    d = dict()
-    email = []
-
+    d = defaultdict(int)
+    alphabetCount = defaultdict(int)
+    wordcount = 0
     try:
         for line in fhand:
-            if line.startswith("From:"):
-                words = line.split()
-                # if len(words)==0:
-                #     continue
-                email.append(words.pop(1))
-                for unique in email:
-                    d[unique]=d.get(unique,0)+1
+            punc_translator = line.maketrans({key: None for key in punctuation})
+            cleanString = line.translate(punc_translator)
+            words = cleanString.lower()
+            words = words.split()
+            for word in words:
+                wordcount +=1
+                d[word] +=1
+                for cha in word:
+                    alphabetCount[cha] +=1
+
     except:
-        print("Nothing was found in the file.")
-    print(email)
-    # MOST_EMAIL_ADDRESS=max(d,key=d.get)
-    # print(MOST_EMAIL_ADDRESS,d[MOST_EMAIL_ADDRESS])
+        print("Error occurred.")
+
+    # File Output
+    print("Here's a summary of the file you have selected: ")
+    if d != {}:
+        print("There were {:,} words.".format(wordcount))
+        print("There were {} distinct words.".format(len(d)))
+        s = sorted(d.items(),key=itemgetter(1), reverse=True)
+        print(s[:25])
+        print("\n")
+        s1 = sorted(alphabetCount.items(),key=itemgetter(1), reverse=True)
+        print(s1)
+        print("\n")
+    else:
+        print("The file you selected is an empty file.")
+    run = input("Press 1 to input another file. Anything else to quit. ")
